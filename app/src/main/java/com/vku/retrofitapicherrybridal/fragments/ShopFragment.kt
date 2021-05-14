4a947f1783e009dee479b8e6a7171cf59b989ca6
trungbmt <phamductrungbmt@gmail.com>
2021-05-14 18:31:09 +0700

    save postion of fragment

--- app/src/main/java/com/vku/retrofitapicherrybridal/fragments/ShopFragment.kt
+++ app/src/main/java/com/vku/retrofitapicherrybridal/fragments/ShopFragment.kt
@@ -2,29 +2,23 @@ package com.vku.retrofitapicherrybridal.fragments
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.vku.retrofitapicherrybridal.AppConfig
 import com.vku.retrofitapicherrybridal.R
 import com.vku.retrofitapicherrybridal.adapter.CategoryAdapter
-import com.vku.retrofitapicherrybridal.client.CategoryClient
-import com.vku.retrofitapicherrybridal.model.Category
-import com.vku.retrofitapicherrybridal.model.CategoryAPI
 import com.vku.retrofitapicherrybridal.viewmodel.ShopViewModel
 import kotlinx.android.synthetic.main.fragment_shop.*
 import kotlinx.android.synthetic.main.fragment_shop.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class ShopFragment : Fragment() {
-
+    var scrollPositionX : Int = 0
+    var scrollPositionY : Int = 0
     private lateinit var rootView : View
     private lateinit var shopViewModel : ShopViewModel
     override fun onCreateView(
@@ -37,11 +31,22 @@ class ShopFragment : Fragment() {
             rootView.rv_category.adapter = categoryAdapter
         })
         rootView = inflater.inflate(R.layout.fragment_shop, container, false)
-
         rootView.rv_category.setHasFixedSize(true)
         rootView.rv_category.layoutManager = GridLayoutManager(this.context, 2)
-
         return rootView
+    }
+    override fun onPause() {
+        super.onPause()
+        // Save ListView state @ onPause
+        scrollPositionX = rootView.scrollX
+        scrollPositionY = rootView.scrollY
+        Log.d("TAG", "saving fragment state = $scrollPositionX / $scrollPositionY")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        rootViewScroll.smoothScrollTo(scrollPositionX,scrollPositionY)
+        Log.d("TAG", "paste fragment state = $scrollPositionX / $scrollPositionY")
     }
 
     companion object {
